fce04770ea8f676c34d89d43ad7ba0b635fb099c
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2014-01-20 11:12:19 +0100

    Initial Commit

---
+++ SPLocationManager/SPLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  SPLocationManager.h
+//  SPLocationManager
+//
+//  Created by Kamil Burczyk on 20.01.2014.
+//  Copyright (c) 2014 Sigmapoint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPLocationManager : NSObject
+
+@end
---
+++ SPLocationManager/SPLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  SPLocationManager.m
+//  SPLocationManager
+//
+//  Created by Kamil Burczyk on 20.01.2014.
+//  Copyright (c) 2014 Sigmapoint. All rights reserved.
+//
+
+#import "SPLocationManager.h"
+
+@implementation SPLocationManager
+
+@end
